f6525af2d137a105a33afc9acc083a1373624c59
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2020-09-11 11:36:32 +0200

    Make /getSigle

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.services.CatService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class MyController {
@@ -12,6 +15,19 @@ public class MyController {
     @ResponseBody
     public String welcome(){
         return "Welcome!!!!!!";
+    }
+
+    @GetMapping ("/getSingle")
+    @ResponseBody
+    public String getSingleFact(){
+        CatService catService = new CatService();
+        try {
+           return catService.getCatData();
+        } catch (IOException e){
+            return "can't access server";
+        }
+
+
     }
 
 }
